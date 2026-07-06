0345bd729b1e00a6b2d81c3d3a645ad0d5cb89b7
INDRONIIL <indroniilsil730@gmail.com>
2026-07-06 21:07:29 +0530

    attach

--- customer.py
+++ customer.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+from sklearn import tree
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("bank.csv")
 print(df.head())
@@ -9,20 +12,44 @@ print(df.head())
 print(df.info())
 print(df.shape)
 
+le = LabelEncoder()
+
+for col in df.columns:
+    if df[col].dtype == 'object':
+        df[col] = le.fit_transform(df[col])
+
+
 X = df.drop('y',axis=1)
 y = df['y']
 
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=42,random_state=42)
 
 model = DecisionTreeClassifier(
-    max_depth=5,
+    criterion="entropy",
     random_state=42,
-    criterion="entropy"
+    max_depth=5
 )
-model.fit(X_train,y_train)
+model.fit(X_train, y_train)
 
 prediction = model.predict(X_test)
 
 print("accuracy_score:",accuracy_score(y_test,prediction))
 print("classification_report:",classification_report(y_test,prediction))
-print("confusion_matrix:",confusion_matrix(y_test,prediction))
+print("confusion_matrix:\n",confusion_matrix(y_test,prediction))
+
+new_customer = [[35,1,2,0,2000,1,0,0,15,5,200,1,-1,0,2,1]]
+prediction = model.predict(new_customer)
+print(prediction)
+
+plt.figure(figsize=(20,10))
+
+tree.plot_tree(
+    model,
+    feature_names=X.columns,
+    class_names=["No","Yes"],
+    filled=True,
+    rounded=True
+)
+
+plt.show()
+
